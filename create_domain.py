1973f545a7fffa0cacc65288b939c59ff049dc7d
Julio Cesar <juliocesarmartinez1344@gmail.com>
2025-07-18 10:14:00 -0500

    script now working

--- create_domain.py
+++ create_domain.py
@@ -1,117 +1,52 @@
-import os
 import sys
+from java.io import FileInputStream
+from java.util import Properties
 
-print "=== CREANDO DOMINIO SOA ==="
+# Cargar propiedades desde archivo
+propFile = sys.argv[1]
+props = Properties()
+props.load(FileInputStream(propFile))
 
-# Validar argumentos
-if len(sys.argv) != 2:
-    print "Uso: wlst.cmd create_domain.py <archivo_propiedades>"
-    exit()
+# Leer propiedades
+domain_name = props.get("DOMAIN_NAME")
+admin_port = int(props.get("ADMIN_PORT"))
+admin_password = props.get("ADMIN_PASSWORD")
+soa_server_name = props.get("SOA_SERVER_NAME")
+soa_port = int(props.get("SOA_PORT"))
+nm_address = props.get("NM_LISTEN_ADDRESS")
+nm_port = int(props.get("NM_PORT"))
 
-properties_file = sys.argv[1]
-print "Cargando propiedades: " + properties_file
+# Cargar plantilla base
+readTemplate("C:/Oracle/Middleware/Oracle_Home/wlserver/common/templates/wls/wls.jar")
 
-# Verificar que el archivo existe
-if not os.path.exists(properties_file):
-    print "ERROR: Archivo no encontrado: " + properties_file
-    exit()
-
-# Cargar propiedades
-try:
-    loadProperties(properties_file)
-    print "Propiedades cargadas correctamente"
-except Exception, e:
-    print "ERROR cargando propiedades: " + str(e)
-    exit()
-
-# Obtener y validar propiedades
-domain_name = get('domain_name')
-port_base_str = get('port_base')
-
-print "DEBUG - domain_name: " + str(domain_name)
-print "DEBUG - port_base_str: " + str(port_base_str)
-
-if domain_name is None or domain_name == '':
-    print "ERROR: domain_name no encontrado en propiedades"
-    exit()
-
-if port_base_str is None or port_base_str == '':
-    print "ERROR: port_base no encontrado en propiedades"
-    exit()
-
-# Convertir puerto a entero
-try:
-    admin_port = int(port_base_str)
-    soa_port = admin_port + 2
-except ValueError:
-    print "ERROR: port_base debe ser un numero, recibido: " + str(port_base_str)
-    exit()
-
-print "Dominio: " + domain_name
-print "Puerto Admin: " + str(admin_port)
-print "Puerto SOA: " + str(soa_port)
-
-# Rutas
-oracle_home = 'C:/Oracle/Middleware/Oracle_Home'
-domain_path = oracle_home + '/user_projects/domains/' + domain_name
-
-print "Ruta dominio: " + domain_path
-
-# Verificar plantillas
-wls_template = oracle_home + '/wlserver/common/templates/wls/wls.jar'
-soa_template = oracle_home + '/soa/common/templates/wls/oracle.soa_template.jar'
-
-print "Verificando plantilla WLS: " + wls_template
-if not os.path.exists(wls_template):
-    print "ERROR: Plantilla WLS no encontrada: " + wls_template
-    exit()
-
-print "Verificando plantilla SOA: " + soa_template
-if not os.path.exists(soa_template):
-    print "ERROR: Plantilla SOA no encontrada: " + soa_template
-    exit()
-
-# Crear dominio
-print "Leyendo plantillas..."
-readTemplate(wls_template)
-addTemplate(soa_template)
-
-print "Configurando AdminServer..."
-cd('/Security/base_domain/User/weblogic')
-cmo.setPassword('Welcome123')
-
-cd('/Server/AdminServer')
+# Configurar AdminServer
+cd('/Servers/AdminServer')
+set('ListenAddress', 'localhost')
 set('ListenPort', admin_port)
 
-print "Creando SOA Server..."
-cd('/')
-create('soa_server1', 'Server')
-cd('/Server/soa_server1')
-set('ListenPort', soa_port)
-setServerGroups(['SOA-MGD-SVRS'])
+# Configurar contraseña del usuario weblogic
+cd('/Security/base_domain/User/weblogic')
+cmo.setPassword(admin_password)
 
-print "Configurando Node Manager..."
+# Crear SOA Server
 cd('/')
-create('LocalMachine', 'Machine')
-cd('/Machine/LocalMachine')
-create('LocalMachine', 'NodeManager')
-cd('NodeManager/LocalMachine')
+create(soa_server_name, 'Server')
+cd('/Servers/' + soa_server_name)
 set('ListenAddress', 'localhost')
-set('NMType', 'PerDomain')
+set('ListenPort', soa_port)
 
-assign('Server', 'AdminServer', 'Machine', 'LocalMachine')
-assign('Server', 'soa_server1', 'Machine', 'LocalMachine')
+# Configurar NodeManager
+cd('/')
+create('MachineScript', 'Machine')
+cd('/Machines/MachineScript')
+create('NodeManager', 'NodeManager')
+cd('/Machines/MachineScript/NodeManager/NodeManager')
+set('ListenAddress', nm_address)
+set('ListenPort', nm_port)
 
-print "Guardando dominio..."
-setOption('DomainName', domain_name)
+# Guardar dominio
 setOption('OverwriteDomain', 'true')
-writeDomain(domain_path)
+writeDomain('C:/Oracle/Middleware/Oracle_Home/user_projects/domains/' + domain_name)
 closeTemplate()
 
-print "=== DOMINIO CREADO EXITOSAMENTE ==="
-print "Nombre: " + domain_name
-print "Ruta: " + domain_path
-print "AdminServer: localhost:" + str(admin_port)
-print "SOA Server: localhost:" + str(soa_port)
-
-exit()
+print('Domain created successfully: ' + domain_name)
